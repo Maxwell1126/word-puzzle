c0020168da6ef701e68e70387134da243f640892
Maxwell Todd <mtmaxtodd@gmail.com>
2022-02-11 19:58:19 -0600

    Added a loop on puzzle.js that creates unique textarea tags in the correct number of rows and columns and is rendered on the dom.

--- src/puzzle.js
+++ src/puzzle.js
@@ -7,11 +7,27 @@ class Puzzle extends Component {
     }
 
     render() {
-
+        
+        let rowDiv=[];
+        let puzzleDiv="";
+        for(let i=1; i<7; i++){
+            console.log("in first loop" + i);
+            let colDiv=[];
+                for(let n=1; n<6; n++){
+                    console.log("in second loop" + n);
+                    colDiv.push(<textarea id={i + `,` + n} className={"textarea"} maxLength={1} rows={1} cols={1}></textarea>);
+                    console.log(colDiv);
+                    if(n == 5){
+                       rowDiv.push(<div id={i}>{colDiv}</div>);
+                       console.log(rowDiv);
+                    }
+                }
+            puzzleDiv = <div>{rowDiv}</div>;
+            }
         return (
             <div>
                 <h1>Word Puzzle</h1>
-                <div><textarea  class={"textarea"} maxLength={1} rows={1} cols={1}></textarea></div>
+                {puzzleDiv}
             </div>
         );
 
