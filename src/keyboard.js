f82623352bbc1d65666a1756fa26aa3d932a8f17
Maxwell Todd <mtmaxtodd@gmail.com>
2022-02-14 15:19:10 -0600

    Added arrays, loops and functions to keyboard.js render to create the keyboard for the app.

--- src/keyboard.js
+++ src/keyboard.js
@@ -7,9 +7,31 @@ class Keyboard extends Component {
     }
 
     render() {
+        let topRowArray = ['Q','W','E','R','T','Y','U','I','O','P'];
+        let middleRowArray = ['A','S','D','F','G','H','J','K','L'];
+        let bottomRowArray = ['Z','X','C','V','B','N','M'];
+        let topRow =[];
+        let middleRow =[];
+        let bottomRow = [];
 
+        function createRow(firstRowArray, secondRowArray, thirdRowArray) {
+            for(let i =0; i < firstRowArray.length; i++){   
+                topRow.push(<button id={firstRowArray[i]}>{firstRowArray[i]}</button>);
+            }
+            for (let i = 0; i < secondRowArray.length; i++) {
+                middleRow.push(<button id={secondRowArray[i]}>{secondRowArray[i]}</button>);
+            }
+            for (let i = 0; i < thirdRowArray.length; i++) {
+                bottomRow.push(<button id={thirdRowArray[i]}>{thirdRowArray[i]}</button>);
+            }
+        }
+        createRow(topRowArray, middleRowArray, bottomRowArray);
         return(
-            <div></div>
+            <div id="keyboard">
+                <div>{topRow}</div>
+                <div>{middleRow}</div>
+                <div><button>Delete</button>{bottomRow}<button>Enter</button></div>
+            </div>
         );
 
     }
