c419439b038abf1018840602a6305ffea79ce8e1
Maxwell Todd <mtmaxtodd@gmail.com>
2022-02-15 17:49:41 -0600

    Added readonly and autofocus attributes for textareas on the puzzle.js file. Added logic to give autofocus to textarea with id 1,1 on render. Added logic to take away autofocus if textarea with id 1,1 has a value.

--- src/puzzle.js
+++ src/puzzle.js
@@ -5,29 +5,31 @@ import Keyboard from './keyboard' ;
 class Puzzle extends Component {
     constructor(props) {
         super(props)
+
     }
 
     render() {
-        
         let rowDiv=[];
         let puzzleDiv="";
         for(let i=1; i<7; i++){
-            console.log("in first loop" + i);
             let colDiv=[];
                 for(let n=1; n<6; n++){
-                    console.log("in second loop" + n);
-                    
-                    console.log(colDiv);
-                    if(n == 5){
-                        colDiv.push(<textarea id={i + `,` + n} className={"textareaLast"} maxLength={1} rows={1} cols={1}></textarea>);
-                       rowDiv.push(<div className={"rowContainer"} id={i}>{colDiv}</div>);
-                       console.log(rowDiv);
+                    if(i==1 && n == 1){
+                        colDiv.push(<textarea  autoFocus="autofocus" readOnly="" id={i + `,` + n} className={"textarea"} maxLength={1} rows={1} cols={1}></textarea>);
+                    }else if(n == 5){
+                        colDiv.push(<textarea readOnly="readonly" id={i + `,` + n} className={"textareaLast"} maxLength={1} rows={1} cols={1}></textarea>);
+                        rowDiv.push(<div className={"rowContainer"} id={i}>{colDiv}</div>);
                     }else{
-                        colDiv.push(<textarea id={i + `,` + n} className={"textarea"} maxLength={1} rows={1} cols={1}></textarea>);
+                        colDiv.push(<textarea readOnly="readonly" id={i + `,` + n} className={"textarea"} maxLength={1} rows={1} cols={1}></textarea>);
                     }
                 }
             puzzleDiv = <div className={"puzzleContainer"}>{rowDiv}</div>;
             }
+
+        if (rowDiv[0].props.children[0].props.value){
+            rowDiv[0].props.children[0].props.autoFocus = "";
+            }
+            
         return (
             <div className="container">
                 <h1>Word Puzzle</h1>
