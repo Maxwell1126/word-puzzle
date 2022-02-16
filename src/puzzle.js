5038a148317933e9a13f214359bfeef538a3ba76
Maxwell Todd <mtmaxtodd@gmail.com>
2022-02-15 23:45:01 -0600

    Changed the component in puzzle.js to become a function in order to use useEffect and add eventlistener. Set the event listener to only allow letters and the backspace.

--- src/puzzle.js
+++ src/puzzle.js
@@ -1,26 +1,38 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect } from 'react';
 import { connect } from 'react-redux';
 import './App.css'
 import Keyboard from './keyboard' ;
-class Puzzle extends Component {
-    constructor(props) {
-        super(props)
+function Puzzle(){
+    React.useEffect(() => {
+    window.addEventListener('keydown', (event) => {
+        if (event.code.charAt(0) != 'K' && event.code != "Backspace") {
+                    event.preventDefault();
+                }
+    });
+}, []);
 
-    }
-
-    render() {
+        // function isCharacterKeyPress(evt) {
+        //     document.addEventListener('keydown', function (event) {
+        //         if (event.code.charAt(0) != 'K') {
+        //             e.preventDefault();
+        //         }
+        //     });
+        // }
         let rowDiv=[];
         let puzzleDiv="";
         for(let i=1; i<7; i++){
             let colDiv=[];
                 for(let n=1; n<6; n++){
                     if(i==1 && n == 1){
-                        colDiv.push(<textarea  autoFocus="autofocus" readOnly="" id={i + `,` + n} className={"textarea"} maxLength={1} rows={1} cols={1}></textarea>);
+                        colDiv.push(<textarea  autoFocus="autofocus" readOnly="" id={i + `,` + n} 
+                            className={"textarea"} maxLength={1} rows={1} cols={1}></textarea>);
                     }else if(n == 5){
-                        colDiv.push(<textarea readOnly="readonly" id={i + `,` + n} className={"textareaLast"} maxLength={1} rows={1} cols={1}></textarea>);
+                        colDiv.push(<textarea readOnly="readonly" id={i + `,` + n} 
+                            className={"textareaLast"} maxLength={1} rows={1} cols={1}></textarea>);
                         rowDiv.push(<div className={"rowContainer"} id={i}>{colDiv}</div>);
                     }else{
-                        colDiv.push(<textarea readOnly="readonly" id={i + `,` + n} className={"textarea"} maxLength={1} rows={1} cols={1}></textarea>);
+                        colDiv.push(<textarea readOnly="readonly" id={i + `,` + n} 
+                            className={"textarea"} maxLength={1} rows={1} cols={1}></textarea>);
                     }
                 }
             puzzleDiv = <div className={"puzzleContainer"}>{rowDiv}</div>;
@@ -41,7 +53,7 @@ class Puzzle extends Component {
     }
 
 
-}
+
 
 const mapStateToProps = reduxStore => ({
     reduxStore
