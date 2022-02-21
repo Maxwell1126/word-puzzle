26b31be3ff0b87dfb44bfc81239d86037a95c597
Maxwell Todd <mtmaxtodd@gmail.com>
2022-02-21 15:04:24 -0600

    Tested code on home.js to be used to eventually implement correctly assigning classnames to the input elements on puzzle.js. Commented out code for now.

--- src/home.js
+++ src/home.js
@@ -10,10 +10,42 @@ let navigate = useNavigate();
 const playButton = (event) => {
     navigate('/puzzle');
 }        
+//     let word = "abcda";
+//     let guess = "abcab";
+//     let inputArray = [<input id={"0"} className="" readOnly="" />,
+//     <input id={"1"} className="" readOnly="" />,
+//     <input id={"2"} className="" readOnly="" />,
+//     <input id={"3"} className="" readOnly="" />,
+//     <input id={"4"} className="" readOnly="" />];
+//  function doingStuff(){
+//      for (let i = 0; i < 5; i++) {
+//          if (word.charAt(i) == guess.charAt(i)) {
+//             document.getElementById(i.toString()).className = "right";
+//          }
+//      }
+
+//      for (let x = 0; x < 5; x++){
+//          for (let n = 0; n < 5; n++){
+//              if (
+//                  word.charAt(n) != guess.charAt(n) &&
+//                  word.charAt(x) != guess.charAt(x) &&
+//                  word.charAt(x) === guess.charAt(n)){
+//                  document.getElementById(n.toString()).className = "misplaced";
+//                  break;
+//             }
+//         }
+//      }
+
+
+//      console.log(document.getElementById("container"));
+//  }
+    
 
         return (
-            <div>
+            <div id ={"container"}>
                 <button onClick={playButton}>Play</button>
+             {/* {inputArray} */}
+             {/* <button onClick={doingStuff}>do stuff</button> */}
             </div>
         );
     }
