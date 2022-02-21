553591ca137cd17d01a4e707c7142f4286cc011a
Maxwell Todd <mtmaxtodd@gmail.com>
2022-02-20 19:22:58 -0600

    Added Database.sql file into the project at the root. Holds the sql queries necessary to start up the initial state of the database.

---
+++ Database.sql
@@ -0,0 +1,7 @@
+CREATE TABLE "current_guess" ("id" serial PRIMARY KEY, "guess" VARCHAR(5));
+CREATE TABLE "word_to_guess"("id" serial PRIMARY KEY, "word" VARCHAR(5), "definition" VARCHAR(500));
+CREATE TABLE "guess_list" ("id" serial PRIMARY KEY, "guess" VARCHAR(5));
+CREATE TABLE "record" ("id" serial PRIMARY KEY, "place" INTEGER);
+
+INSERT INTO "current_guess"("guess")
+VALUES ('');
